0a9168b49c4e8f10837f425de20d1e782c878292
sueroku <eraltndusn@gmail.com>
2024-06-10 17:44:47 +0900

    fix

--- src/C01Basic/C07Array.java
+++ src/C01Basic/C07Array.java
@@ -10,7 +10,7 @@ public class C07Array {
 //
 ////        배열 표현식2 : 배열 선언 후 할당
 //        int[] intArr2 = new int[5];
-//        ;
+//
 //        intArr2[0] = 1;
 //        intArr2[1] = 2;
 //        intArr2[2] = 3;
@@ -27,12 +27,12 @@ public class C07Array {
 //        int[] intArr3 = new int[]{1, 2, 3, 4};
 //
 ////      표현식4 : 불가 -> 배열의 길이는 사전에 지정되어야함
-////        int[] intArr4 new int[];
+////        int[] intArr4 = new int[];
 //
-//        String[] stArr = new String[5];
+//        String[] stArr = new String[5]; // 초기화시 null로 초기화
 //        stArr[0] = "hello";
 //        stArr[1] = "java";
-////        null pointer exception
+////        null pointer exception 발생 가능성..
 ////        System.out.println(stArr[2].length());
 //
 //        String[] stArr2 = new String[5];
@@ -104,16 +104,225 @@ public class C07Array {
 //        String[] st_arr = {"hello", "hi", "bye","goodmorning"};
 //        Arrays.sort(st_arr, Comparator.reverseOrder());
 
-//        기본형 타입은 comparator 로 처리 불가
-        int[] arr = {5,1,2,7,3,1,2};
-//        Arrays.sort(arr);
-//        방법 1. int 오름차순 정렬 후 베열 뒤집기 // Integer[] -> comarator -> int[]에 넣기
-//        방법 2. streamapi lamda를 활용한 내림차순 정렬
-        int[] answer = Arrays.stream(arr) // arr를 대상으로 stream 개체(원본의 복사본 정도로 이해하라) 생성
-                .boxed() //int요소를 integer로 형변환 시키는 메서드
-                .sorted(Comparator.reverseOrder())
-                .mapToInt(a->a) // Integer를 int로 형변환
-                .toArray(); // 배열로 변환
+////        기본형 타입은 comparator 로 처리 불가
+//        int[] arr = {5,1,2,7,3,1,2};
+////        Arrays.sort(arr);
+////        방법 1. int 오름차순 정렬 후 베열 뒤집기 // Integer[] -> comarator -> int[]에 넣기
+////        방법 2. streamapi lamda를 활용한 내림차순 정렬
+//        int[] answer = Arrays.stream(arr) // arr를 대상으로 stream 개체(원본의 복사본 정도로 이해하라) 생성
+//                .boxed() //int요소를 integer로 형변환 시키는 메서드
+//                .sorted(Comparator.reverseOrder())
+//                .mapToInt(a->a) // Integer를 int로 형변환
+//                .toArray(); // 배열로 변환
+
+////        k번째 수 : 프로그래머스
+////          선택정렬 알고리즘 (최솟값 찾기, 그때그때 비교 바꾸기)
+//        int[] arr = {17,20,19,25,12};
+//        for(int i=0;i<arr.length-1;i++){
+//            int index=i;
+//            for(int j=i+1; j<arr.length;j++){
+//                if(arr[index] > arr[j]){
+//                    index = j;
+//                }
+//            }
+//            int temp = arr[i];
+//            arr[i] = arr[index];
+//            arr[index]=temp;
+//        }
+
+//        for(int i=0;i<arr.length-1;i++){
+//            for(int j=i+1; j<arr.length;j++){
+//                if(arr[i] > arr[j]){
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j]=temp;
+//                }
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(arr));
+
+////        숫자 조합의 합 : 모두 각기 다른 숫자의 배열이 있을 때,
+////        만들 수 있는 2개의 조합의 합을 출력
+//        int[] intArr = {10,20,30,40,50,60};
+//        for(int i=0;i<intArr.length-1;i++){ // -1안해줘도 된다.
+//            for(int j=i+1;j<intArr.length;j++){
+//                System.out.println(intArr[i] + " " + intArr[j]);
+//            }
+//        }
+
+////        배열의 복사 : copyOf(배열, length) 시작은 0// copyOfRange(배열,시작, 끝)
+//        int[] arr = {10,20,30,40,50};
+//        int[] newArr = Arrays.copyOf(arr, 3);
+//        System.out.println(Arrays.toString(newArr));
+//        newArr = Arrays.copyOf(arr, 10);
+//        System.out.println(Arrays.toString(newArr));
+//        int[] newArr2 = Arrays.copyOfRange(arr, 1,4);
+//        System.out.println(Arrays.toString(newArr2));
+
+//        배열의 중복제거
+//        int[] temp = {10,10,5,7,40,40,10,10,50}; // 나중에는 list set
+////        옆숫자와 같은지 아닌지 비교 // 새로운 배열의 길이는 넉넉하게 temp길이로 // 추후 copyofrange
+//        Arrays.sort(temp);
+//        int[] newTemp = new int[temp.length];
+//        int index = 0;
+//        for(int i=0; i<temp.length;i++){
+//            if(i==temp.length-1){
+//                if(temp[i]!=temp[i-1]){
+//                    newTemp[index] = temp[i];
+//                    break;
+//                }
+//            }
+//            if(temp[i]!=temp[i+1]){
+//                newTemp[index] = temp[i];
+//                index++;
+//            }
+//        }
+//        System.out.println(Arrays.toString(newTemp));
+
+//        Arrays.sort(temp);
+//        int[] newTemp = new int[temp.length];
+//        int index = 0;
+//        for(int i=0; i<temp.length;i++){
+//            if(i==0 || temp[i]!=temp[i-1]){ // 처음조건부터 들어가기 때문에 다음조건이 문제가 되지 않는다.
+//                newTemp[index] = temp[i];
+//                index++;
+//            }
+//        }
+//        int[] answer = Arrays.copyOfRange(newTemp,0,index);
+//        System.out.println(Arrays.toString(answer));
+
+
+//        프로그래머스 두개의 값 더하기
+        int[] numbers = new int[]{2,1,3,4,1};
+//        Arrays.sort(numbers);
+//        int[] temp = new int[numbers.length*(numbers.length-1)];
+//        int index =0;
+//        for(int i=0; i<numbers.length;i++){
+//            for(int j=i+1;j<numbers.length;j++){
+//                temp[index] = numbers[i]+numbers[j];
+//                index++;
+//            }
+//        }
+//        int[] newTemp = Arrays.copyOfRange(temp,0,index);
+//        Arrays.sort(newTemp);
+//        int[] last = new int[newTemp.length];
+//        index = 0;
+//        for(int i=0; i<newTemp.length;i++){
+//            if(i==0 || newTemp[i]!=newTemp[i-1]){
+//                last[index] = newTemp[i];
+//                index++;
+//            }
+//        }
+//        int[] answer = Arrays.copyOfRange(last,0,index);
+//        System.out.println(Arrays.toString(answer));
+
+//        int[] temp = new int[numbers.length*(numbers.length-1)];
+//        int count = 0;
+//        for(int i=0;i<numbers.length-1;i++){
+//            for(int j=i+1;j<numbers.length;j++){
+//                temp[count] = numbers[i] + numbers[j];
+//                count++;
+//            }
+//        }
+//        temp = Arrays.copyOf(temp, count);
+//        Arrays.sort(temp);
+//        int[] newTemp = new int[temp.length];
+//        int index = 0;
+//        for(int i=0; i<temp.length;i++){
+//            if(i==0 || temp[i]!=temp[i-1]){ // 처음조건부터 들어가기 때문에 다음조건이 문제가 되지 않는다.
+//                newTemp[index] = temp[i];
+//                index++;
+//            }
+//        }
+//        int[] answer = Arrays.copyOfRange(newTemp,0,index); // answer = Arrays.copyOf(newTemp, index);
+//        System.out.println(Arrays.toString(answer));
+
+////        배열의 검색
+//        int[] arr = {5,3,1,8,7};
+//        int answer =-1;
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i]==8){
+//                answer=i;
+//                break;
+//            }
+//        }
+//        System.out.println(answer);
+
+////        이진검색(Binary search) - 이분탐색
+////        복잡도 O(logN)
+//        int[] arr = {1,3,6,8,9,11,15};
+////         사전에 오름차순 정렬이 되어 있어야 이진검색 가능 //  전제 조건 : 정렬되어있어야한다.
+//        System.out.println(Arrays.binarySearch(arr,15));
+
+////        배열간 비교
+//        int[] arr1 = {10,20,30};
+//        int[] arr2 = {10,20,30};
+////        Arrays.equals() : 값과 순서까지 동일해야 true
+//        System.out.println(Arrays.equals(arr1,arr2));
+
+////        2차원 배열 선언과 할당
+//        int[][] arr = new int[2][3];
+//        arr[0][0] = 1; // 쭈욱
+////      리터럴방식으로 할당
+//        int[][] arr2 = {{1,2,3},{4,5,6}};
+////      2차원 배열의 출력
+//        System.out.println(Arrays.deepToString(arr2));
+
+////        2차원 가변배열 선언후 할당
+//        int[][] arr = new int[2][];
+//        arr[0] = new int[3];
+//        arr[1] = new int[4];
+//
+////        가변배열 리터럴 방식
+//        int[][] arr2 = {{10,20},{10,20,30}};
+
+////        [3][4]사이즈 배열을 선언한 뒤
+////        1-12 숫자값 각 배열에 할당 후 출력
+////        {{1,2,3,4},{5,6,7,8},{9,10,11,12}}
+//        int n =12;
+//        int[][] arr = new int[n/4][4];
+//        int input = 1;
+//        for(int i=0;i<n/4 ; i++){
+//            for(int j=0;j<4;j++){
+//                arr[i][j] = input;
+//                input++;
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(arr));
+//        int count=1;
+//        int[][] temp = new int[3][4];
+//        for(int i=0;i<temp.length;i++){
+//            for(int j=0; j<temp[i].length;j++){
+//                temp[i][j] = count;
+//                count++;
+//            }
+//        }
+//        System.out.println(Arrays.deepToString(temp));
+
+////        프로그래머스 행렬의 덧셈
+//        int[][] arr1 = new int[2][2];
+//        int[][] arr2 = new int[2][2];
+//        int[][] answer = new int[arr1.length][arr1[0].length];
+//        for(int i=0;i<arr1.length;i++){
+//            for(int j=0;j<arr1[i].length;j++){
+//                answer[i][j] = arr1[i][j] + arr2[i][j];
+//            }
+//        }
+
+////        가변배열실습 : 행의 길이 (전체 배열의 길이) 5 => {{10},{10,20},{10,20,30},{10,20,30,40},{10,20,30,40,50}}
+//        int[][] arr = new int[5][];
+//        for(int i=0;i<arr.length;i++){
+//            arr[i] = new int[i+1];
+//            for(int j=0;j<arr[i].length;j++){
+////                arr[i][j] = (j+1)*10;
+//                arr[i][j] = (i+1)*10; // 문제를 잘못 받아 적었넹ㅎ
+//            }
+//
+//        }
+//        System.out.println(Arrays.deepToString(arr));
+
+
 
 
 
---
+++ src/C01Basic/C09List.java
@@ -0,0 +1,173 @@
+package C01Basic;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class C09List {
+    public static void main(String[] args) {
+////        list 선언 방법
+////        ArrayList<타입명> 변수명 = new ArrayList<타입명>();
+//        ArrayList<String> myList1 = new ArrayList<>();
+//        ArrayList<String> myList2 = new ArrayList<String>();
+//
+////        가장일반적인 list 선언방법
+////        왼쪽엔 인터페이스(기능없이 선언만 되어있는 요소), 오른쪽엔 구현체(실제 기능이 있는 실체. 이 클래스의 기능들을 사용한다.)(클래스)
+//        List<String> myList3 = new ArrayList<>();
+//        Queue<String> myQue = new LinkedList<>();
+//
+////        초기값 생성 방법 1. 하나씩 add
+//        myList3.add("java");
+//        myList3.add("python");
+//        myList3.add("c++");
+//
+////        초기값 생성 방법 2. 리스트를 주입
+////        Arrays.asList : 배열을 리스트로 변환하는 매서드
+//        String[] myArr = {"java","python","c++"};
+//        List<String> myList4 = new ArrayList<>(Arrays.asList(myArr)); // (myArr 말고 myList3넣을 수 있다. 괄호 안에 리스트를 넣을 수 있다.)
+//        List<String> myList6 = new ArrayList<>(List.of(myArr));
+//
+//        int[] intArr = {10,20,30};
+//        List<Integer> myList5 = new ArrayList<>();// int는 안되지만 int[]배열은 참조형변수라 가능 타입에 넣을 수 있다. // (Arrays.asList(myArr))안된다. 형변환 이슈..!
+//        for(int i :intArr){
+//            myList5.add(i);
+//        }
+
+//        List<Integer> myList = new ArrayList<>();
+//        myList.add(10);
+//        myList.add(20);
+////        System.out.println(myList);
+////        add(index, 값) : 중간에 값 삽입 = 성능 떨어진다. 밀리느라 : arraylist의 특징
+//        myList.add(0,30);
+////        System.out.println(myList);
+//
+//        List<Integer> myList2 = new ArrayList<>();
+//        myList2.add(1);
+//        myList2.add(2);
+////        특정리스트의 요소를 모두 add
+//        myList.addAll(myList2);
+////        System.out.println(myList);
+//
+////        get 특정 위치의 요소를 반환
+////        System.out.println(myList.get(0));
+//
+////        for문을 돌려 전체 list 출력
+////        size() 리스트의 길이(개수) 반환
+////        for(int i=0;i<myList.size();i++){
+////            System.out.println(myList.get(i));
+////        }
+//
+////        remove : 요소 삭제
+////        1) index를 통한 삭제 2) value를 통한 삭제
+//        System.out.println(myList);
+//        myList.remove(0);
+//        System.out.println(myList);
+//        myList.remove(Integer.valueOf(10));
+//        System.out.println(myList);
+
+////        set(index, value) : index 위치에 value값 setting(변경)
+//        List<Integer> myList = new ArrayList<>();
+//        myList.add(10); myList.add(20); myList.add(30);
+//        System.out.println(myList);
+//        myList.set(myList.size()-1, 10);
+//        System.out.println(myList);
+//
+////        indexOf : 특정값이 몇번째 index에 위치한지 return
+////        가장 먼저 나오는 값의 index return
+//        System.out.println(myList.indexOf(10));
+
+//        전체삭제 : clear()
+
+////        isEmpty : 값이 비었는지 안비었는지
+////        contains : 특정값이 있는지 없는지 확인
+//        List<Integer> list = new ArrayList<>(); // 비어있으나 null은 아니다
+//        System.out.println(list.isEmpty());
+////        String hello = null;
+////        System.out.println(hello.isEmpty()); // 오류
+//
+//        list.add(10);
+//        System.out.println(list.contains(20));
+//
+////        이중 리스트 (리스트 안에 리스트)
+//        List<List<Integer>> myList = new ArrayList<>();
+//        myList.add(new ArrayList<>());
+//        myList.add(new ArrayList<>());
+//        myList.get(0).add(10);
+//        myList.get(0).add(20);
+//        myList.get(1).add(1);
+//        myList.get(1).add(2);
+//        System.out.println(myList);
+//
+////        리스트 안에 배열 // 배열 사이즈 2,3,4개 짜리 배열 3개 들어가는 것으로 add
+//        List<int[]> myList2 = new ArrayList<>();
+//        myList2.add(new int[]{1,2}); // new int[2]
+//        myList2.add(new int[]{10,20,30});
+//        myList2.add(new int[]{100,200,300,400});
+
+////        리스트 정렬 : collections.sort() 리스트 객체.sort()
+//        List<Integer> myList = new ArrayList<>();
+//        myList.add(5);
+//        myList.add(3);
+//        myList.add(2);
+//        myList.add(1);
+//        myList.add(4);
+////        방법1. collections.sort()
+//        Collections.sort((myList));
+//        Collections.sort(myList, Collections.reverseOrder());
+////        방법2. 리스트객체.sort()
+//        myList.sort(Comparator.naturalOrder());
+//        myList.sort(Comparator.reverseOrder());
+
+//        프로그래머스 n의 배수 고르기
+//        int n = 3;
+//        int[] numlist = {4,5,6,7,8,9,10,11,12};
+//        List<Integer> myList = new ArrayList<>(Arrays.asList(numlist));
+//        for(int i=0 ;i<myList.size();i++){
+//            if(myList.get(i)%n !=0){
+//                myList.remove(i);
+//            }
+//        }
+//
+//        int[] answer = myList.toArray(new int[myList.size()]);
+
+////        프로그래머스 두 개 뽑아서 더하기
+//        List<Integer> myList = new ArrayList<>();
+//        int[] numbers = {4,5,6,7,8,9,10,11,12};
+//        for(int i=0;i<numbers.length;i++){
+//            for(int j=i+1;j<numbers.length;j++){
+//                if(myList.contains(numbers[i]+numbers[j])){
+//                    continue;
+//                }else {
+//                    myList.add(numbers[i]+numbers[j]);
+//                }
+//            }
+//        }
+//        Collections.sort(myList);
+//        int[] answer =new int[myList.size()];
+//        for(int i=0;i<myList.size();i++){
+//            answer[i] = myList.get(i);
+//        }
+//        System.out.println(myList);
+
+//        배열과 list 간의 변환
+//        1. String 배열을 list<String>로
+        String[] stArr = {"java", "python","c++"};
+//        1-1. Arrays.asList
+        List<String> stList = new ArrayList<>(Arrays.asList(stArr));
+//        1-2. for문을 통해 담기
+//        1-3. streaAPI (참고만)
+        List<String> stList2 = Arrays.stream(stArr).collect(Collectors.toList());
+
+//        2. List<String>을 String 배열로 변환
+//        2-1. for문을 통해 담기
+//        2-2. toArray
+        String[] stArr2 = stList.toArray(new String[stList.size()]);
+
+//        3. int배열을 list<Integer>로 변환
+//        3-1. for문을 통해 담기
+//        3-2. streamAPI 사용
+        int[] intArr = {10,20,30,40};
+        List<Integer> intList = Arrays.stream(intArr).boxed().collect(Collectors.toList());
+
+
+    }
+}
